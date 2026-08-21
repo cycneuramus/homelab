37e108963f1ccd8d00e2ffd5294aa2af13e54ee3
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2026-08-21 08:25:24 +0200

    ytdl-sub: Update yt-dlp on start

--- ytdl-sub/entrypoint.sh
+++ ytdl-sub/entrypoint.sh
@@ -24,6 +24,21 @@ on_error() {
 
 trap 'on_error' err
 
+# https://github.com/jmbannon/ytdl-sub/blob/5b76e628077ef5b92265f51788f3ec43bb14a869/docker/root/custom-cont-init.d/defaults#L29C1-L41C3
+if [ "$UPDATE_YT_DLP_ON_START" == "stable" ]; then
+	echo "UPDATE_YT_DLP_ON_START is set to stable, attempting to update to a new stable version of yt-dlp if it exists."
+	python3 -m pip install -U "yt-dlp[default]"
+elif [ "$UPDATE_YT_DLP_ON_START" == "nightly" ]; then
+	echo "UPDATE_YT_DLP_ON_START is set to nightly, attempting to update to the latest nightly version of yt-dlp."
+	python3 -m pip install -U --pre "yt-dlp[default]"
+elif [ "$UPDATE_YT_DLP_ON_START" == "master" ]; then
+	echo "UPDATE_YT_DLP_ON_START is set to master, pulling yt-dlp's latest commit for install."
+	python3 -m pip install -U pip hatchling wheel
+	python3 -m pip install --force-reinstall "yt-dlp[default] @ https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz"
+else
+	echo "UPDATE_YT_DLP_ON_START is not set, using packaged version."
+fi
+
 ytdl-sub \
 	--log-level info \
 	--config /local/config.yaml \
